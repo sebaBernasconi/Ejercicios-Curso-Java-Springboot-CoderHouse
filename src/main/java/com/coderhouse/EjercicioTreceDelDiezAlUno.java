f965dd86eb12e28df987e5fcdc1a707c9ad4bbe3
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-03-02 19:17:49 -0300

    Ejercicio 13 terminado

---
+++ src/main/java/com/coderhouse/EjercicioTreceDelDiezAlUno.java
@@ -0,0 +1,12 @@
+package com.coderhouse;
+
+public class EjercicioTreceDelDiezAlUno {
+
+    public static void main(String[] args) {
+        System.out.println("A continuacion se listaran los nros del 10 al 1");
+
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
