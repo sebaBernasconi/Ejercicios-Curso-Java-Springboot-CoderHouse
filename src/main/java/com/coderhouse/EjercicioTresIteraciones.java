c3091df28fad9e01caaf82e63af8c5a2e9602d33
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-03-02 18:08:56 -0300

    Ejercicio 3 terminado

---
+++ src/main/java/com/coderhouse/EjercicioTresIteraciones.java
@@ -0,0 +1,12 @@
+package com.coderhouse;
+
+public class EjercicioTresIteraciones {
+
+    public static void main(String[] args) {
+        System.out.println("A continuacion se imprimiran los nros del 1 al 10");
+
+        for (int i = 1 ; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
