a3b6c7bdcabdd848ace91cc84098b1b1017b9639
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-03-02 17:58:49 -0300

    Ejercicio 1 terminado

---
+++ src/main/java/com/coderhouse/Ejercicio1.java
@@ -0,0 +1,23 @@
+package com.coderhouse;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        int numeroEntero = 11;
+        double numeroDouble = 23.04;
+        char caracter = 'a';
+        boolean estaLloviendo = true;
+
+        System.out.println("El valor de la variable entera numeroEntero es --> " + numeroEntero);
+        System.out.println();
+
+        System.out.println("El valor de la variable double numeroDouble es --> " + numeroDouble);
+        System.out.println();
+
+        System.out.println("El valor de la variable char caracter es --> '" + caracter + "'");
+        System.out.println();
+
+        System.out.println("El valor de la variable booleana estaLloviendo es --> " + estaLloviendo);
+        System.out.println();
+    }
+}
